e41b12b2ee7a5b112f094e92a4509389de9e1391
SabineHAMAI <sahamai-ext@airfrance.fr>
2023-03-13 10:50:26 +0100

    clean
    cleaning

--- src/main/java/com/function/Function.java
+++ src/main/java/com/function/Function.java
@@ -13,11 +13,6 @@ import com.microsoft.azure.functions.*;
  */
 public class Function {
 
-    /**
-     * This function listens at endpoint "/api/HttpExample". Two ways to invoke it using "curl" command in bash:
-     * 1. curl -d "HTTP Body" {your host}/api/HttpExample
-     * 2. curl "{your host}/api/HttpExample?name=HTTP%20Query"
-     */
     @FunctionName("HttpExample")
     public HttpResponseMessage run(
             @HttpTrigger(name = "req", methods = {HttpMethod.GET, HttpMethod.POST}, authLevel = AuthorizationLevel.ANONYMOUS) HttpRequestMessage<Optional<String>> request,
